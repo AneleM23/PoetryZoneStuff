fc1458c2a5d4bc8c0763f0d116b6766d75a0c76b
umsomi70 <111692216+umsomi70@users.noreply.github.com>
2024-03-12 20:41:01 +0200

    .

--- Assets/Scripts/Scripts_AmaFS/LevelManager.cs
+++ Assets/Scripts/Scripts_AmaFS/LevelManager.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State
+{
+    Start,
+    Poems,
+    Levels,
+    Play,
+    End,
+}
+
 public class LevelManager : MonoBehaviour
 {
+
+    [SerializeField] AudioSource themeSong;
+
     // Start is called before the first frame update
     void Start()
     {
