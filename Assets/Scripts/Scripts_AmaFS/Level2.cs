3735f99fc56e81dfe9632b071d0d79f77af24340
umsomi70 <111692216+umsomi70@users.noreply.github.com>
2024-03-24 22:26:24 +0200

    .

--- Assets/Scripts/Scripts_AmaFS/Level2.cs
+++ Assets/Scripts/Scripts_AmaFS/Level2.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Phase
+{
+    1, 2, 3, 4, 5, 6, 7
+}
+
 public class Level2 : MonoBehaviour
 {
+    Phase phase  ;
+
     // Start is called before the first frame update
     void Start()
     {
