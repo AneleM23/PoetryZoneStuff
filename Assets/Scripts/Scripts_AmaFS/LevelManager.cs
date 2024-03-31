116c4c347598fc86d35c9162397ef0a4af509c5e
umsomi70 <111692216+umsomi70@users.noreply.github.com>
2024-03-31 22:09:39 +0200

    .

--- Assets/Scripts/Scripts_AmaFS/LevelManager.cs
+++ Assets/Scripts/Scripts_AmaFS/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum State
 {
@@ -15,16 +16,31 @@ public class LevelManager : MonoBehaviour
 {
 
     [SerializeField] AudioSource themeSong;
+    [SerializeField] State state;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = State.Start;
+        themeSong.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if     (SceneManager.GetActiveScene().name == "Level 1" || SceneManager.GetActiveScene().name == "Level 2" || SceneManager.GetActiveScene().name == "Level 3" || SceneManager.GetActiveScene().name == "Level 4" || SceneManager.GetActiveScene().name == "Level 5")
+        {
+            state = State.Play;
+        }
+
+        if(SceneManager.GetActiveScene().name == "LevelSelect")
+        {
+            state = State.Levels;
+        }
+
+        if (state == State.Play)
+        {
+            themeSong.Stop();
+        }
     }
 }
--- Assets/Scripts/Scripts_AmaFS/Scene_Manager.cs
+++ Assets/Scripts/Scripts_AmaFS/Scene_Manager.cs
@@ -24,4 +24,29 @@ public class Scene_Manager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
+
+    public void LoadLevel5()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
 }
